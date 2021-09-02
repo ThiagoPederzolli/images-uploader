1039aaf9b36146a60f9a138ff7e55d3d99545b54
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-09-02 12:45:29 -0300

    logic completed, missing style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,31 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+// type Image = {
+//   title: string;
+//   description: string;
+//   url: string;
+//   ts: number;
+//   id: string;
+// };
+
+// type imagesRequestReturn = {
+//   data: Image[];
+//   after: string;
+// };
+
 export default function Home(): JSX.Element {
+  // const imagesRequest = async ({
+  //   pageParam = null,
+  // }): Promise<imagesRequestReturn> => {
+  //   const response = await api.get('/api/images', {
+  //     params: {
+  //       after: pageParam?.after,
+  //     },
+  //   });
+  //   return response.data;
+  // };
+
   const {
     data,
     isLoading,
@@ -18,26 +42,45 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get('/api/images', {
+        params: {
+          after: pageParam?.after,
+        },
+      });
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => (lastPage?.after ? lastPage : null),
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages?.flatMap(image => image.data);
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
-
+  if (isError) {
+    return <Error />;
+  }
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            disabled={!hasNextPage}
+            onClick={() => fetchNextPage()}
+            isLoading={isFetchingNextPage}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
